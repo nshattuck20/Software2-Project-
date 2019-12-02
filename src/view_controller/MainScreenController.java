f65cc4dd1aedcac9282fee700bb7b1e5da3e0400
Nick <nshattuck20@gmail.com>
2019-12-02 13:49:00 -0800

    Revert "Names will populate in the name column but I don't think it's correctly done.  It's something though..."
    This reverts commit 8204c20b2734c080b499d232785c12f6faf7bc45.

--- src/view_controller/MainScreenController.java
+++ src/view_controller/MainScreenController.java
@@ -35,7 +35,7 @@ public class MainScreenController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        //This is a comment....
+
         ObservableList<Schedule> customers = FXCollections.observableArrayList();
         ObservableList<Customer> customerNames = FXCollections.observableArrayList(); 
         //Lambas for columns 
