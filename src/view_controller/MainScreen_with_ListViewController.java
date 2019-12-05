729b0524779111ef42603f5b44ef9b5760dce937
Nick <nshattuck20@gmail.com>
2019-12-04 16:18:33 -0800

    Schedule class refactored. Now called "TableRow"

--- src/view_controller/MainScreen_with_ListViewController.java
+++ src/view_controller/MainScreen_with_ListViewController.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package view_controller;
 
 import java.io.IOException;
@@ -5,11 +10,7 @@ import java.net.URL;
 import java.sql.SQLException;
 import java.util.Optional;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.application.Platform;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -18,89 +19,64 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
 import javafx.stage.Stage;
-import softwareII.Implementation.CustomerImplementation;
 import softwareII.Implementation.DBConnection;
-import softwareII.Model.Customer;
-import softwareII.Model.TableRow;
 
 /**
  * FXML Controller class
  *
  * @author Nick Shattuck
  */
-public class MainScreenController implements Initializable {
+public class MainScreen_with_ListViewController implements Initializable {
 
     @FXML
-    private TableView<Customer> table;
-
+    private DatePicker datePicker;
     @FXML
-    private TableColumn<Customer, String> customerNameColumn;
-
-    //Extra space for table column names 
-    //Buttons
+    private Button createCustomerBtn;
+    @FXML
+    private Button editCustomerBtn;
+    @FXML
+    private Button deleteCustomerBtn;
+    @FXML
+    private Button addApptBtn;
+    @FXML
+    private Button editApptBtn;
+    @FXML
+    private Button deleteApptBtn;
+    @FXML
+    private Button sortByMonthBtn;
+    @FXML
+    private Button sortByUserBtn;
+    @FXML
+    private Button sortByReportBtn;
     @FXML
     private Button logoutBtn;
-
     @FXML
     private Button exitBtn;
+    @FXML
+    private RadioButton weekRadioBtn;
+    @FXML
+    private RadioButton MonthRadioBtn;
+    @FXML
+    private ListView<?> listview;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
 
-       // ObservableList<Schedule> customers = FXCollections.observableArrayList();
-        ObservableList<Customer> customers = FXCollections.observableArrayList();
-        
-        //Lambas for columns 
-        customerNameColumn.setCellValueFactory(cellData -> {
-            return cellData.getValue().getCustomerName();
-        });
-
-//        startTimeColumn.setCellValueFactory(cellData -> {
-//            return cellData.getValue().getStarTime();
-//        });
-//        endTimeColumn.setCellValueFactory(cellData -> {
-//            return cellData.getValue().getEndTime();
-//        });
-//        cityColumn.setCellValueFactory(cellData -> {
-//            return cellData.getValue().getCity();
-//        });
-//
-//        countryColumn.setCellValueFactory(cellData -> {
-//            return cellData.getValue().getCountry();
-//        });
-//        appointmentTypeColumn.setCellValueFactory(cellData -> {
-//            return cellData.getValue().getAppointmentType();
-//        });
-
-
-
-        try {
-            customers.clear(); 
-            customers.addAll(CustomerImplementation.getAllCustomerNames());
-            
-            table.setItems(customers);
-//            TableRow s = new TableRow((ObservableValue<Appointment>) customers); 
-//            appointmentTable.setItems((ObservableList<Schedule>) s);
-        } catch (Exception ex) {
-            Logger.getLogger(MainScreenController.class.getName()).log(Level.SEVERE, null, ex);
-            // appointmentTable.setItems(appointments);
-        }
-
-    }
-//Buttons
-    @FXML
+ @FXML
     public void logoutButton(ActionEvent event) throws IOException, SQLException, Exception {
         System.out.println("Logout Button Clicked!");
-        Alert alert = new Alert(AlertType.WARNING);
+        Alert alert = new Alert(Alert.AlertType.WARNING);
         alert.setContentText("Are you sure you want to logout?");
         alert.setHeaderText("Confirm Logout");
         Optional<ButtonType> confirm = alert.showAndWait();
@@ -122,7 +98,7 @@ public class MainScreenController implements Initializable {
         //When the user clicks the Exit button, show an alert asking for confirmation. 
         //The key difference between this button and the logout button is that this button 
         //Closes the entire program, so make sure the user understands it. 
-        Alert alert = new Alert(AlertType.WARNING);
+        Alert alert = new Alert(Alert.AlertType.WARNING);
         alert.setContentText("Are you sure you want to exit? Doing so will close the entire program!");
         alert.setHeaderText("Confirm Exit");
         Optional<ButtonType> confirm = alert.showAndWait();
@@ -153,4 +129,5 @@ public class MainScreenController implements Initializable {
             createCustomerStage.show();
     }
 
+    
 }
